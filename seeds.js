8cbc9a324e6c1a61bca5c4199929cd7982d6f967
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-09-25 14:50:15 -0400

    added/seeded data with more items like promotional add-ons as well as using Faker to generate a promotional price compared to original price

--- seeds.js
+++ seeds.js
@@ -1,9 +1,38 @@
 import { faker } from "@faker-js/faker";
+import fs from "fs";
 
 export default function createRandomProducts() {
   // !Commerce departments
   const departments = [];
   const products = {};
+  const promotionalAddOn = [
+    "Bestseller",
+    "Just In",
+    "Available in a store near you",
+    "Available in Artic Sea Blue ",
+    "Customizable",
+    "Lowest Price Online",
+    "Free Shipping",
+    "Comes with Bonus Gift",
+    "Similar to your recents",
+    "Trending",
+    "Most gifted",
+    "Most Watched",
+    "Limited Edition",
+    "Premium Value",
+    "Coming Soon",
+  ];
+  var productPromotionAddOn = Math.floor(
+    Math.random() * promotionalAddOn.length
+  );
+  // console.log(
+  //   productPromotionAddOn,
+  //   "out of ",
+  //   promotionalAddOn.length,
+  //   "promo is: ",
+  //   promotionalAddOn[productPromotionAddOn]
+  // );
+
   for (let i = 0; i < 6; i++) {
     let department = faker.commerce.department();
     if (!departments.includes(department)) {
@@ -11,7 +40,7 @@ export default function createRandomProducts() {
     }
   }
   console.log(departments);
-  //   for each department x 15 products
+  //   for each department x 15 products (0-14)
   for (let i = 0; i < departments.length; i++) {
     var Product = {};
     for (let j = 0; j < 15; j++) {
@@ -21,6 +50,13 @@ export default function createRandomProducts() {
       var productPrice = faker.commerce.price(5, 125, 0, "$");
       var productDescription = faker.commerce.productDescription();
       var productDepartment = departments[i];
+      var productPromoPrice = faker.commerce.price(
+        10,
+        productPrice - 2,
+        0,
+        "$"
+      );
+      var productPromotionAddOn = promotionalAddOn[productPromotionAddOn];
       var productImg = faker.image.imageUrl(
         640,
         480,
@@ -37,7 +73,9 @@ export default function createRandomProducts() {
         productMaterial,
         productDepartment,
         productDescription,
+        productPromotionAddOn,
         productPrice,
+        productPromoPrice,
         productRating,
         productImg,
       };
@@ -45,7 +83,25 @@ export default function createRandomProducts() {
     }
     products[departments[i]] = Product;
   }
-  return products;
+  try {
+    fs.writeFileSync("data.json", JSON.stringify(products), function (err) {
+      if (!err) {
+        console.log(
+          "Seeding data to data.json writting is complete! Have fun shopping ~!"
+        );
+      } else {
+        // else means there's error!
+        console.error(
+          "There was an error in the writting process itself. data.json IS NOT seeded properly... Try again!"
+        );
+      }
+    });
+  } catch (e) {
+    console.error(
+      e,
+      "There was a problem with writing/ finalizing the seeding process for each department 15 items (0-14)"
+    );
+  }
 }
 
 createRandomProducts();
