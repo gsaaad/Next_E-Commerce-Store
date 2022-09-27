2d6a5da1d4457b6e9b5c79b5c1a7e2a6828707d6
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-09-27 12:14:38 -0400

    added other pages of departments, which are based on the seeded data

---
+++ pages/collections/Health.js
@@ -0,0 +1,23 @@
+import * as React from "react";
+import { useRouter } from "next/router";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import Navigation from "../../components/Navigation";
+import CollectionProducts from "../../components/CollectionProducts";
+import BreadcrumbsNavigation from "../../components/BreadcrumbsNavigation";
+import PRODUCTS from "../../data.json";
+
+export default function Games() {
+  const router = useRouter();
+  const collectionName = router.route.split("/")[2];
+
+  return (
+    <Box>
+      <Navigation products={PRODUCTS} />
+      <Container maxWidth="lg">
+        <BreadcrumbsNavigation title={collectionName} />
+        <CollectionProducts products={PRODUCTS} />
+      </Container>
+    </Box>
+  );
+}
