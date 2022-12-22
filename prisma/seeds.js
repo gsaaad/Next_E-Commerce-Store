e8268aa0d8ccc690b8ee64aab516e9e9354dde3f
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-12-22 11:57:04 -0500

    s

--- prisma/seeds.js
+++ prisma/seeds.js
@@ -1,6 +1,6 @@
 import { faker } from "@faker-js/faker";
-
-function createDepartments() {
+import fs from "fs";
+function createProducts() {
   // !Commerce departments
   const departments = [];
   var loopCount = 0;
@@ -15,8 +15,7 @@ function createDepartments() {
   console.log("List of departments:", departments);
   console.log("Length of departments", departments.length);
   console.log("Loop ran", loopCount);
-}
-function createProducts() {
+
   const promotionalAddOn = [
     "Bestseller",
     "Just In",
@@ -37,74 +36,74 @@ function createProducts() {
   var productPromotionAddOn = Math.floor(
     Math.random() * promotionalAddOn.length
   );
-  //   for each department x 16 products (0-15)
-  // for (let i = 0; i < departments.length; i++) {
-  //   var Product = {};
-  //   for (let j = 0; j < 16; j++) {
-  //     var productId = faker.datatype.uuid().split("-")[1];
-  //     var productName = faker.commerce.productName();
-  //     var productMaterial = productName.split(" ")[1];
-  //     var productPrice = faker.commerce.price(37, 99, 0, "$");
-  //     var productPromoPrice = faker.commerce.price(
-  //       30,
-  //       productPrice.slice(1) - 2,
-  //       0,
-  //       "$"
-  //     );
-  //     console.log(productPrice, productPromoPrice);
+  var products = [];
+  // for each department x 16 products (0-15)
+  for (let i = 0; i < departments.length; i++) {
+    var Product = {};
+    for (let j = 0; j < 16; j++) {
+      var productId = faker.datatype.uuid().split("-")[1];
+      var productName = faker.commerce.productName();
+      var productMaterial = productName.split(" ")[1];
+      var productPrice = faker.commerce.price(37, 99, 0, "$");
+      var productPromoPrice = faker.commerce.price(
+        30,
+        productPrice.slice(1) - 2,
+        0,
+        "$"
+      );
 
-  //     var productDescription = faker.commerce.productDescription();
-  //     var productDepartment = departments[i];
+      var productDescription = faker.commerce.productDescription();
+      var productDepartment = departments[i];
 
-  //     var productPromotionAddOn = promotionalAddOn[productPromotionAddOn];
-  //     var productImg = faker.image.imageUrl(
-  //       640,
-  //       480,
-  //       `${productName.split(" ")[2]}`
-  //     );
-  //     var productRating = faker.datatype.float({
-  //       min: 2.8,
-  //       max: 5,
-  //       precision: 0.1,
-  //     });
-  //     var product = {
-  //       productId,
-  //       productName,
-  //       productMaterial,
-  //       productDepartment,
-  //       productDescription,
-  //       productPromotionAddOn,
-  //       productPrice,
-  //       productPromoPrice,
-  //       productRating,
-  //       productImg,
-  //     };
-  //     Product[j] = product;
-  //   }
-  //   products[departments[i]] = Product;
-  // }
-  // try {
-  //   fs.writeFileSync("data.json", JSON.stringify(products), function (err) {
-  //     if (!err) {
-  //       console.log(
-  //         "Seeding data to data.json writting is complete! Have fun shopping ~!"
-  //       );
-  //     } else {
-  //       // else means there's error!
-  //       console.error(
-  //         "There was an error in the writting process itself. data.json IS NOT seeded properly... Try again!"
-  //       );
-  //     }
-  //   });
-  //   console.log(
-  //     "Seeding data to data.json writting is complete! Have fun shopping ~!"
-  //   );
-  // } catch (e) {
-  //   console.error(
-  //     e,
-  //     "There was a problem with writing/ finalizing the seeding process for each department 15 items (0-14)"
-  //   );
-  // }
+      var productPromotionAddOn = promotionalAddOn[productPromotionAddOn];
+      var productImg = faker.image.imageUrl(
+        640,
+        480,
+        `${productName.split(" ")[2]}`
+      );
+      var productRating = faker.datatype.float({
+        min: 2.8,
+        max: 5,
+        precision: 0.1,
+      });
+      var product = {
+        productId,
+        productName,
+        productMaterial,
+        productDepartment,
+        productDescription,
+        productPromotionAddOn,
+        productPrice,
+        productPromoPrice,
+        productRating,
+        productImg,
+      };
+      Product[j] = product;
+    }
+    products[departments[i]] = Product;
+  }
+  try {
+    fs.writeFileSync("data.json", JSON.stringify(products), function (err) {
+      if (!err) {
+        console.log(
+          "Seeding data to data.json writting is complete! Have fun shopping ~!"
+        );
+      } else {
+        // else means there's error!
+        console.error(
+          "There was an error in the writting process itself. data.json IS NOT seeded properly... Try again!"
+        );
+      }
+    });
+    console.log(
+      "Seeding data to data.json writting is complete! Have fun shopping ~!"
+    );
+  } catch (e) {
+    console.error(
+      e,
+      "There was a problem with writing/ finalizing the seeding process for each department 15 items (0-14)"
+    );
+  }
 }
-
-export default { createDepartments, createProducts };
+createProducts();
+export default { createProducts };
